006e9135bd9e29025dabee702f221f29babc44db
Christopherplm <christopherplopes@gmail.com>
2025-10-15 20:40:02 -0300

    pequena correção

--- Desenhos/QuadradoCheio.java
+++ Desenhos/QuadradoCheio.java
@@ -4,7 +4,7 @@ public class QuadradoCheio {
     public static void main(String[] args) {
         
         int linha = 4;
-        int coluna = 4; 
+        int coluna = 5; 
 
         for(int i = 0; i <= linha; i++ ){
             for(int j= 0; j <= coluna; j++){
